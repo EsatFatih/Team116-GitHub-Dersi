2c0734f812c25311fd3a31c18c2967a376477586
EsatFatih <esata091@gmail.com>
2023-04-26 13:37:24 +0300

    2.commit

--- src/day01/C02.java
+++ src/day01/C02.java
@@ -9,6 +9,30 @@ public class C02 {
     Bu kalsorde her yedek almak istedihimizde commit yapmaliyiz
     asagıdaki uc komut bizim dosyalarimizi her yedeklemek istedigimizde kullanilir
 
-    1- git add .    : son kaydedilen versiyondan sonra projede yapilan degisiklikleri belirler
+    1- git add .    : son kaydedilen versiyondan sonra
+                    projede yapilan degisiklikleri belirler
+
+    2- git commit -m "commit ismi" :
+                    projenin son durumunu bir versiyon olarak kaydeder
+                    burada "" icinde yazilan commit isminin anlamli olmasinda fayda vardir
+                    ilerleyen asamalarda geri donus yapmak istersek hangi commit e donmek
+                    isteyecegimizi acıklamalardan bulabiliriz
+
+    git ile ilisiklendirdigimiz dosyadaki class isimleri git durumuna gore renklenir:
+
+    kirmizi :bu class herhangi bir versiyona eklenmedi
+    mavi:    bu dosya bir versiyona eklendi ama sonradan degisiklik yapilmis
+    siyah : bu class bir versiyonda son haliyle yedeklendi
+            sonrasinda hic bir degisiklik yok
+
+    3. git push : local repoda commit ettigimiz dosyalarimizi uzak masaustu(GitHub)ne yollamak
+                icin kullanilir
+
+                Local bilgisayarda olusturdugumuz bir repo'yu gitHub'a yollamak ve orada
+                yedeklemek istiyorsak oncelikle GitHub hesabi olusturmamiz ve sonrasinda
+                projemizi gonderecegimiz Github reposunu hazirlamamiz gerekir
+
+                Github'da yeni bir repository olusturdugumuzda
+                ilk kullanimda local repo ile remote repo arasinda baglanti kurmamiz gerekir
      */
 }
