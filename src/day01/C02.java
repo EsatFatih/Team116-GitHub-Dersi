1e924d474ff81cc69fce4e0a7597a3bcebc7bd70
EsatFatih <esata091@gmail.com>
2023-04-26 13:01:11 +0300

    ilk commit

---
+++ src/day01/C02.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class C02 {
+    /*
+    git init komutu, bir workingDiretory'u
+    git ile irtibatlandırır. bu komut her proje icin bir kere
+    yapılır. bu komut ile birlikte git icinde bulunulan klasoru kontrol etmeye baslar.
+
+    Bu kalsorde her yedek almak istedihimizde commit yapmaliyiz
+    asagıdaki uc komut bizim dosyalarimizi her yedeklemek istedigimizde kullanilir
+
+    1- git add .    : son kaydedilen versiyondan sonra projede yapilan degisiklikleri belirler
+     */
+}
